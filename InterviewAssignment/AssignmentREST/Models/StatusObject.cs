634439bbc96e2ba78480dde9aeb97efb9df41caf
Wouter <wouter@elastiq-connect.com>
2021-08-01 18:05:32 +0200

    Started transforming test methods, models and classes to fulfill the requirements. Added a Thread.Sleep with random input to GUIDController"
    ;

---
+++ InterviewAssignment/AssignmentREST/Models/StatusObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssignmentREST.Models
+{
+    
+    public class StatusObject
+    {
+
+        public long GUID { get; set; }
+
+    }
+}
